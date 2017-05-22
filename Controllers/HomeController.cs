342c25d47de8f0a992e5554321293c1f06a6de33
JordanS4nderson <jlhabc@hotmail.co.uk>
2017-05-22 12:11:38 +0100

    commit
    commit from jordan

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JordanHall_Mayborn.Controllers
+{
+    public class HomeController : Controller
+    {
+        //retrives index page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Goes to about page
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        //Goes to contract page
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public void Save(string firstName, string lastName)
+        {
+            //saves to the user details folder under user
+            using (StreamWriter writer = new StreamWriter(Server.MapPath("/UserDetails/user.txt"), true))
+            {
+                writer.WriteLine($"{firstName} {lastName},");
+            }
+
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JordanHall_Mayborn.MvcApplication" Language="C#" %>
